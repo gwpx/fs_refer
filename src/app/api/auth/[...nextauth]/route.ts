ee2caefe4f055367bd4ea856e039dc821c6b0308
Gary <garywupx@gmail.com>
2025-08-09 17:03:31 +0100

    redeploy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import LinkedIn from "next-auth/providers/linkedin";
 
 const handler = NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     LinkedIn({
       clientId: process.env.LINKEDIN_CLIENT_ID ?? "",
